c438f43fbbee24fc1b8cdb6271e078ccfe931acd
toufiqnuur <taufik2017yk@gmail.com>
2021-09-25 21:09:36 +0700

    add: vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,15 @@
 //import sveltePreprocess from 'svelte-preprocess'
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
+import vercel from '@sveltejs/adapter-vercel';
 
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 	preprocess: [mdsvex(mdsvexConfig)],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: vercel()
 	}
 };
 
