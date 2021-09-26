2f57355a18aee42421e962c7a99c3bc7637bc8d3
toufiqnuur <taufik2017yk@gmail.com>
2021-09-26 10:58:53 +0700

    fix: remove unused attr

--- src/lib/function/kombinasi.js
+++ src/lib/function/kombinasi.js
@@ -11,8 +11,8 @@ const kombinasi = (n, r) => {
 		let _n = makeArray(1, n);
 		_n = _n.filter((v) => makeArray(1, r).indexOf(v) == -1);
 		let _r = makeArray(1, n - r);
-		_n = _n?.reduce((a, b) => a * b);
-		_r = _r?.reduce((a, b) => a * b);
+		_n = _n.reduce((a, b) => a * b);
+		_r = _r.reduce((a, b) => a * b);
 		return _n / _r;
 	}
 	return 0;
